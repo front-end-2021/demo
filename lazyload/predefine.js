1a4949ff746c5ae336a9cc1a7210780a283a5ec6
baodainguyen <baodaihd89@gmail.com>
2023-06-18 17:32:44 +0700

    load html file with fetch api

--- lazyload/predefine.js
+++ lazyload/predefine.js
@@ -1,4 +1,4 @@
-//loadHTML('#grp-templates', `/demo/template.html`)
+loadHTML('#grp-templates', `https://github.com/front-end-2021/demo/blob/main/lazyload/template.html`)
 
 function loadHTML(selector, path) {
     fetch(path).then(response => response.text())
