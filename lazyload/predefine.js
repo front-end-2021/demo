ffa00915f422d00277ded3dfb728a4b24309f065
baodainguyen <baodaihd89@gmail.com>
2023-06-18 17:35:30 +0700

    refactor code cause CORS policy

--- lazyload/predefine.js
+++ lazyload/predefine.js
@@ -1,4 +1,4 @@
-loadHTML('#grp-templates', `https://github.com/front-end-2021/demo/blob/main/lazyload/template.html`)
+//loadHTML('#grp-templates', `https://github.com/front-end-2021/demo/blob/main/lazyload/template.html`)     // blocked by CORS
 
 function loadHTML(selector, path) {
     fetch(path).then(response => response.text())
