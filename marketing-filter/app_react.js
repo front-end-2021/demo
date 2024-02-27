e278014a121a7e1dc57d88453292f6210144c970
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2024-02-27 13:37:01 +0700

    replace kendoui by semantic ui react in react on marketing filter

--- marketing-filter/app_react.js
+++ marketing-filter/app_react.js
@@ -1,117 +1,17 @@
-const { useState, useEffect, StrictMode } = React
+const { useState, useEffect, StrictMode, useMemo } = React
+const { Dropdown } = semanticUIReact
 
 const ReactFltRow = ({ ii, dfilter, onDelRow }) => {
     const [cOperand, setOperand] = useState(dfilter.getBlock(ii).Operand)
     const [cType, setCType] = useState(dfilter.getBlock(ii).Type)
     const [cIds, setCIds] = useState(dfilter.getBlock(ii).Ids)
-    useEffect(() => {
-        const row = dfilter.getBlock(ii)
-        const evnt = () => {
-            setOperand(row.Operand)
-            setCType(row.Type)
-            setCIds([...row.Ids])
+    const sourceIds = useMemo(() => {
+        const lstSrc = []
+        for (let jj = 0; jj < cIds.length; jj++) {
+            const lSrc = getSourceIds(jj)
+            lstSrc.push(lSrc.map(src => { return { key: src.Id, text: src.Name, value: src.Id } }))
         }
-        // console.log('mounted/updated')
-        const $wrap = $(`[c-criterial="${ii}"]`)
-        dfilter.setEvent(ii, evnt)
-        renderUiIds()
-        renOperandControl()
-        renTypeControl()
-        renderIdsDropdownList()
-        return async () => {
-            // console.log('before mount/update/unmount')
-            const lstTask = []
-            for (let jj = cIds.length - 1; -1 < jj; jj--) {
-                const taskId = new Promise(() => {
-                    const $input = $wrap.find(`[c-index="${jj}"]`)
-                    const control = $input.data("kendoDropDownList")
-                    if (control) control.destroy()
-                    $wrap.find('.ccrite-grp-ids').empty();
-                })
-                lstTask.push(taskId)
-            }
-            const taskOprand = new Promise(() => {
-                const $input = $wrap.find(`[c-operand="${ii}"]`)
-                const control = $input.data("kendoDropDownList")
-                if (control) control.destroy()
-            })
-            lstTask.push(taskOprand)
-            const taskType = new Promise(() => {
-                const $input = $wrap.find(`[c-type="${ii}"]`)
-                const control = $input.data("kendoDropDownList")
-                if (control) control.destroy()
-            })
-            lstTask.push(taskType)
-            Promise.all(lstTask).then(() => { dfilter.removeEvent(ii) })
-        }
-        function renderUiIds() {
-            $wrap.find('.ccrite-grp-ids').empty();
-            let ui = ''
-            for (let jj = 0; jj < cIds.length; jj++) {
-                ui += `<input c-index="${jj}" style="width: 240px" />`
-            }
-            $wrap.find('.ccrite-grp-ids').append(ui)
-        }
-        function renOperandControl() {
-            const $operand = $wrap.find(`[c-operand="${ii}"]`)
-            const operandChange = (e) => {
-                const tOprnd = e.sender.value();
-                row.Operand = parseInt(tOprnd);
-                if (cOperand != row.Operand)
-                    setOperand(row.Operand)
-            }
-            $operand.kendoDropDownList({
-                dataTextField: "Name",
-                dataValueField: "Id",
-                dataSource: ii > 0 ? Operands : [mFilterBy],
-                enable: ii < 1 ? false : true,
-                value: cOperand,
-                change: operandChange
-            })
-        }
-        function renTypeControl() {
-            const $type = $wrap.find(`[c-type="${ii}"]`)
-            const typeChange = (e) => {
-                const tType = e.sender.value()
-                row.Type = parseInt(tType)
-                row.Ids = getInitIds(row.Type);
-                const isChangeTyp = cType != row.Type
-                if (isChangeTyp) setCType(row.Type)
-                const isChangeId = cIds.join() != row.Ids.join()
-                if (isChangeId) setCIds([...row.Ids])
-                if (isChangeTyp || isChangeId)
-                    dfilter.setDSource(ii)
-            }
-            $type.kendoDropDownList({
-                dataTextField: "Name",
-                dataValueField: "Id",
-                dataSource: mType,
-                value: cType,
-                change: typeChange
-            })
-        }
-        function renderIdsDropdownList() {
-            for (let jj = 0; jj < row.Ids.length; jj++) {
-                const _id = row.Ids[jj]
-                const $input = $wrap.find(`[c-index="${jj}"]`)
-                const idChange = (e) => {
-                    let id = e.sender.value()
-                    id = parseInt(id);
-                    if (row.Ids[jj] != id) {
-                        row.Ids[jj] = id;
-                        setCIds([...row.Ids])
-                    }
-                    dfilter.setDSource(ii)
-                }
-                $input.kendoDropDownList({
-                    dataTextField: "Name",
-                    dataValueField: "Id",
-                    dataSource: getSourceIds(jj),
-                    value: _id,
-                    change: idChange
-                })
-            }
-        }
+        return lstSrc
         function getSourceIds(index) {
             let lst = []
             switch (cType) {
@@ -166,6 +66,7 @@ const ReactFltRow = ({ ii, dfilter, onDelRow }) => {
                     }
                     return lst
             }
+            return lst
             function getPrdGroups(lst) {
                 const landRgnId = closestIds(ii, 1)
                 const rgnIds = landRgnId.length > 1 ? [landRgnId[1]] : [0]
@@ -200,16 +101,259 @@ const ReactFltRow = ({ ii, dfilter, onDelRow }) => {
                 return []
             }
         }
+    }, [ii, cIds])
+    useEffect(() => {
+        const row = dfilter.getBlock(ii)
+        const evnt = () => {
+            setOperand(row.Operand)
+            setCType(row.Type)
+            setCIds([...row.Ids])
+        }
+        // console.log('mounted/updated')
+        //  const $wrap = $(`[c-criterial="${ii}"]`)
+        dfilter.setEvent(ii, evnt)
+        //  renderUiIds()
+        // renOperandControl()
+        // renTypeControl()
+        // renderIdsDropdownList()
+        // return async () => {
+        // console.log('before mount/update/unmount')
+        // const lstTask = []
+        // for (let jj = cIds.length - 1; -1 < jj; jj--) {
+        //     const taskId = new Promise(() => {
+        //         const $input = $wrap.find(`[c-index="${jj}"]`)
+        //         const control = $input.data("kendoDropDownList")
+        //         if (control) control.destroy()
+        //         $wrap.find('.ccrite-grp-ids').empty();
+        //     })
+        //     lstTask.push(taskId)
+        // }
+        // const taskOprand = new Promise(() => {
+        //     const $input = $wrap.find(`[c-operand="${ii}"]`)
+        //     const control = $input.data("kendoDropDownList")
+        //     if (control) control.destroy()
+        // })
+        // lstTask.push(taskOprand)
+        // const taskType = new Promise(() => {
+        //     const $input = $wrap.find(`[c-type="${ii}"]`)
+        //     const control = $input.data("kendoDropDownList")
+        //     if (control) control.destroy()
+        // })
+        // lstTask.push(taskType)
+        // Promise.all(lstTask).then(() => { dfilter.removeEvent(ii) })
+        // }
+        // function renderUiIds() {
+        //     $wrap.find('.ccrite-grp-ids').empty();
+        //     let ui = ''
+        //     for (let jj = 0; jj < cIds.length; jj++) {
+        //         ui += `<input c-index="${jj}" style="width: 240px" />`
+        //     }
+        //     $wrap.find('.ccrite-grp-ids').append(ui)
+        // }
+        // function renOperandControl() {
+        //     const $operand = $wrap.find(`[c-operand="${ii}"]`)
+        //     const operandChange = (e) => {
+        //         const tOprnd = e.sender.value();
+        //         row.Operand = parseInt(tOprnd);
+        //         if (cOperand != row.Operand)
+        //             setOperand(row.Operand)
+        //     }
+        //     $operand.kendoDropDownList({
+        //         dataTextField: "Name",
+        //         dataValueField: "Id",
+        //         dataSource: ii > 0 ? Operands : [mFilterBy],
+        //         enable: ii < 1 ? false : true,
+        //         value: cOperand,
+        //         change: operandChange
+        //     })
+        // }
+        // function renTypeControl() {
+        //     const $type = $wrap.find(`[c-type="${ii}"]`)
+        //     const typeChange = (e) => {
+        //         const tType = e.sender.value()
+        //         row.Type = parseInt(tType)
+        //         row.Ids = getInitIds(row.Type);
+        //         const isChangeTyp = cType != row.Type
+        //         if (isChangeTyp) setCType(row.Type)
+        //         const isChangeId = cIds.join() != row.Ids.join()
+        //         if (isChangeId) setCIds([...row.Ids])
+        //         if (isChangeTyp || isChangeId)
+        //             dfilter.setDSource(ii)
+        //     }
+        //     $type.kendoDropDownList({
+        //         dataTextField: "Name",
+        //         dataValueField: "Id",
+        //         dataSource: mType,
+        //         value: cType,
+        //         change: typeChange
+        //     })
+        // }
+        // function renderIdsDropdownList() {
+        //     for (let jj = 0; jj < row.Ids.length; jj++) {
+        //         const _id = row.Ids[jj]
+        //         const $input = $wrap.find(`[c-index="${jj}"]`)
+        //         const idChange = (e) => {
+        //             let id = e.sender.value()
+        //             id = parseInt(id);
+        //             if (row.Ids[jj] != id) {
+        //                 row.Ids[jj] = id;
+        //                 setCIds([...row.Ids])
+        //             }
+        //             dfilter.setDSource(ii)
+        //         }
+        //         $input.kendoDropDownList({
+        //             dataTextField: "Name",
+        //             dataValueField: "Id",
+        //             dataSource: getSourceIds(jj),
+        //             value: _id,
+        //             change: idChange
+        //         })
+        //     }
+        // // }
+        // function getSourceIds(index) {
+        //     let lst = []
+        //     switch (cType) {
+        //         case 1: // Land/Region
+        //             lst = [lType[0]]
+        //             if (0 == index) {
+        //                 Lands.forEach(land => lst.push(land))
+        //                 return lst
+        //             }
+        //             const landId = cIds[0]
+        //             return getRegions(landId, lst)
+        //         case 2: // Product groups/Product
+        //             switch (index) {
+        //                 case 0:     // Product group
+        //                     lst = [lType[1]]
+        //                     return getPrdGroups(lst)
+        //                 case 1:     // product
+        //                     const pgId = cIds[index - 1]
+        //                     const lstPrdGrpId = pgId < 0 ? getPrdGroups([]).map(x => x.Id) : [pgId]
+        //                     lst = [lType[2]]
+        //                     return getProductsIn(lstPrdGrpId, lst)
+        //                 case 2:     // sub product
+        //                     const prdId = cIds[index - 1]
+        //                     lst = [lType[3]]
+        //                     return getSubProducts(prdId, lst)
+        //             }
+        //             return []
+        //         case 3:
+        //             switch (index) {
+        //                 case 0:     // stake holder group | sub market
+        //                     lst = [lType[4]]
+        //                     const mkIds = closestMarketIds()
+        //                     return getSubmarket(mkIds, lst)
+        //                 case 1: return [lType[5]]    // Contact person
+        //                 case 2: return [lType[6]]    // Sub contact
+        //             }
+        //             return []
+        //         case 5: // Market segments/Stakeholder groups
+        //             if (index == 0) {
+        //                 lst = [lType[10], lType[0]]
+        //                 const landRegnId = closestIds(ii, 1)
+        //                 const landId = landRegnId.length ? landRegnId[0] : -1
+        //                 return getMarkets(landId, lst)
+        //             }
+        //             const marketId = cIds[0]
+        //             lst = [lType[11]]
+        //             if (marketId < 0) return lst
+        //             for (let ii = 0; ii < StakeholderGroups.length; ii++) {
+        //                 const stk = StakeholderGroups[ii]
+        //                 if (0 < marketId && marketId != stk.MarketId) continue
+        //                 lst.push(stk)
+        //             }
+        //             return lst
+        //     }
+        //     function getPrdGroups(lst) {
+        //         const landRgnId = closestIds(ii, 1)
+        //         const rgnIds = landRgnId.length > 1 ? [landRgnId[1]] : [0]
+        //         return getProductGroups(rgnIds, lst)
+        //     }
+        //     function closestMarketIds() {
+        //         for (let jj = ii - 1; -1 < jj; jj--) {
+        //             const prevCrite = dfilter.getBlock(jj)
+        //             if (1 == prevCrite.Type) {
+        //                 const landId = prevCrite.Ids[0]
+        //                 if (landId < 0) return []
+        //                 if (landId == 0) {
+        //                     const markets = getMarkets(0, [])
+        //                     return markets.map(x => x.Id)
+        //                 }
+        //                 const markets = getMarkets(landId, [])
+        //                 return markets.map(x => x.Id)
+        //             }
+        //             if (5 == prevCrite.Type) {
+        //                 const mkrId = prevCrite.Ids[0]
+        //                 const markets = getMarkets(mkrId, [])
+        //                 return markets.map(x => x.Id)
+        //             }
+        //         }
+        //         return []
+        //     }
+        //     function closestIds(ii, type) {
+        //         for (let jj = ii - 1; -1 < jj; jj--) {
+        //             const crite = dfilter.getBlock(jj)
+        //             if (crite.Type == type) return crite.Ids
+        //         }
+        //         return []
+        //     }
+        // }
     }, [cOperand, cType, cIds])
     const delRow = () => {
         onDelRow(ii)
     }
+    const onChangeOperand = (ii, obj) => {
+        const newOperand = obj.value
+        if (cOperand != newOperand) {
+            const row = dfilter.getBlock(ii)
+            setOperand(newOperand)
+            row.Operand = newOperand;
+        }
+    }
+    const onChangeType = (ii, obj) => {
+        const newType = obj.value
+        if (cType != newType) {
+            const row = dfilter.getBlock(ii)
+            row.Type = newType
+            setCType(newType)
+            const newIds = getInitIds(newType);
+            setCIds(newIds)
+            row.Ids = newIds
+            dfilter.setDSource(ii)
+        }
+    }
+    const onChangeId = (ii, jj, obj) => {
+        const newId = obj.value
+        const oldId = cIds[jj]
+        if (newId != oldId) {
+            const row = dfilter.getBlock(ii)
+            cIds[jj] = newId
+            const newIds = [...cIds]
+            setCIds(newIds)
+            row.Ids = newIds
+        }
+    }
     const clssBtnDel = `btn btn-primary rounded-circle bi bi-trash-fill btn-del-crite-${ii} btn-del-crite`
     return (
         <div c-criterial={ii}>
-            <input c-operand={ii} style={{ width: '96px' }} />
-            <input c-type={ii} style={{ width: '270px' }} />
-            <span className="ccrite-grp-ids"></span>
+            {/* <input c-operand={ii} style={{ width: '96px' }} /> */}
+            {ii < 1 ? (<Dropdown disabled value={cOperand}
+                options={[{ key: mFilterBy.id, text: mFilterBy.Name, value: mFilterBy.Id }]} />) : (
+                <Dropdown selection placeholder='Operand Control'
+                    onChange={(e, oj) => onChangeOperand(ii, oj)}
+                    options={Operands.map(x => { return { key: x.Id, text: x.Name, value: x.Id } })}
+                    value={cOperand} />)}
+            <Dropdown selection placeholder='Type Control'
+                onChange={(e, oj) => onChangeType(ii, oj)}
+                options={mType.map(x => { return { key: x.Id, text: x.Name, value: x.Id } })}
+                value={cType} />
+            {/* <input c-type={ii} style={{ width: '270px' }} /> */}
+            {Array.isArray(cIds) ? cIds.map((_id, jj) => {
+                return <Dropdown value={_id} options={sourceIds[jj]}
+                    key={'id-item-idx_' + jj} selection
+                    onChange={(e, oj) => onChangeId(ii, jj, oj)} />
+            }) : null}
+            {/* <span className="ccrite-grp-ids"></span> */}
             {0 < ii ? <button className={clssBtnDel} type="button"
                 onClick={() => delRow()}></button> : null}
         </div>
