c40cbd10385cdf5e4747c7f1e435c4aeae56f8e5
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2024-03-26 17:21:57 +0700

    add new table in user database in dotnet8 project

--- dotnet8/Entries/Info.cs
+++ dotnet8/Entries/Info.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Web.Api.Entries {
     public class Account {
         public long Id { get; set; }
@@ -5,5 +7,13 @@ namespace Web.Api.Entries {
         public required string Email { get; set; }
         public required string Phone { get; set; }
         public DateTime DoB { get; set; }
+        public required string Password { get; set; }
+    }
+    public class Category{
+        public long Id { get; set; }
+        public required string Name { get; set; }
+        
+        [ForeignKey("CategoryId")]
+        public ICollection<Account>? Users { get; set; }
     }
 }
