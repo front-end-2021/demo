1efb66bc9be1c9958e23ef5648b5f0a954e841d9
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2024-04-01 10:31:22 +0700

    remove comment

--- dotnet8/Entries/Api/ApiEntryTodo.cs
+++ dotnet8/Entries/Api/ApiEntryTodo.cs
@@ -7,17 +7,5 @@ namespace Web.Api.Entries
         public DateTime? Start { get; set; }
         public DateTime? End { get; set; }
         public List<long> AccountIds { get; set; }
-        // public EntryGoal()
-        // {
-        //     AccountIds = [];
-        // }
-        // public EntryGoal(Goal g)
-        // {
-        //     Id = g.Id;
-        //     Name = g.Name;
-        //     Start = g.Start;
-        //     End = g.End;
-        //     AccountIds = [];
-        // }
     }
 }
