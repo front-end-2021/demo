c7e796c0781ffc007c2070750958b4ecd8ebc185
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2024-06-18 08:57:28 +0700

    seperate html file in vue3x

--- vue3x/common.js
+++ vue3x/common.js
@@ -78,3 +78,31 @@ export const CriterialType = [
     `Supplier`,
     `Hidden elements`,
 ]
+export const includeHTML = (path) => {
+    const items = document.getElementsByClassName("dnbimporthtml");
+    path = path.trim()
+    for (let i = 0; i < items.length; i++) {
+        const elmnt = items[i];   /*search for elements with a certain atrribute:*/
+        const file = elmnt.getAttribute("dnbpath");
+        if (!file) continue
+        if (file.trim() !== path) continue
+        
+        const xhr = new XMLHttpRequest(); /* Make an HTTP request using the attribute value as the file name: */
+        return new Promise((resolve, reject) => {
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == XMLHttpRequest.DONE) {
+                    if (xhr.status === 0 || (200 <= xhr.status && xhr.status < 400)) {
+                        // The request has been completed successfully
+                        document.body.innerHTML += `\n ${xhr.responseText}`
+                        resolve(xhr.responseText)
+                    } else {
+                        reject(xhr.status) // There has been an error with the request!
+                    }
+                    elmnt.removeAttribute("dnbpath"); /* Remove the attribute, and call xhr function once more: */
+                }
+            }
+            xhr.open("GET", file, true);
+            xhr.send();
+        })
+    }
+}
--- vue3x/main-app.js
+++ vue3x/main-app.js
@@ -1,4 +1,4 @@
-import { getRandomInt, getTxtBy } from './common.js'
+import { getRandomInt, includeHTML } from './common.js'
 import CompNav from './comp-nav.js'
 import { DropSelect } from './comp-global.js'
 import dnbStore from './main-store.js'
@@ -6,91 +6,100 @@ import { AppModal } from './comp-modal.js'
 import { MarketPage } from './comp-page.js'
 const { createApp } = Vue
 
-const app = createApp({
-    name: `app-main`,
-    components: {
-        'comp-nav': CompNav,
-        'page-market': MarketPage,
-    },
-    data() {
-        return {
-            UserInfo: {
-                img: `https://allimages.sgp1.digitaloceanspaces.com/tipeduvn/2022/01/1642393308_940_Hinh-Anh-Girl-Xinh-Viet-Nam-Dep-De-Thuong-Cute.jpg`,
-                header: `Profile`,
-                content: {
-                    head: `We've auto-chosen a profile image for you.`,
-                    description: `We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a>
+const sPath = `./semantics.html`
+includeHTML(sPath).then(rText => {
+    const app = createApp({
+        name: `app-main`,
+        components: {
+            'comp-nav': CompNav,
+            'page-market': MarketPage,
+        },
+        data() {
+            return {
+                UserInfo: {
+                    img: `https://allimages.sgp1.digitaloceanspaces.com/tipeduvn/2022/01/1642393308_940_Hinh-Anh-Girl-Xinh-Viet-Nam-Dep-De-Thuong-Cute.jpg`,
+                    header: `Profile`,
+                    content: {
+                        head: `We've auto-chosen a profile image for you.`,
+                        description: `We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a>
                     image associated with your registered e-mail address.</p>
                   <p>Is it okay to use this photo?`
+                    }
+                },
+                Users: ['Unassigned', 'Bill Gate', 'Elon Musk', 'Larry Page'],
+                UserAssign: '',
+
+                IndexProject: 0,
+                IndexPage: 0,
+            }
+        },
+        computed: {
+            //  CLang() { return this.$store.getters.activeLang },
+            CProject() {
+                const prj = this.$store.state.Projects[this.IndexProject]
+                if (typeof prj != 'object' || Object.is(prj, null)) return {}
+                return prj
+            },
+            CompPage() {
+                switch (this.IndexPage) {
+                    case 0: return 'page-market';
+                    default: break;
                 }
             },
-            Users: ['Unassigned', 'Bill Gate', 'Elon Musk', 'Larry Page'],
-            UserAssign: '',
 
-            IndexProject: 0,
-            IndexPage: 0,
-        }
-    },
-    computed: {
-      //  CLang() { return this.$store.getters.activeLang },
-        CProject() {
-            const prj = this.$store.state.Projects[this.IndexProject]
-            if (typeof prj != 'object' || Object.is(prj, null)) return {}
-            return prj
+            // #region trace dev
+            message() { return this.$store.getters.message },
+            // #endregion
         },
-        CompPage() {
-            switch (this.IndexPage) {
-                case 0: return 'page-market';
-                default: break;
+        methods: {
+            selectPage(index) { this.IndexPage = index },
+            increment() {
+                this.$store.commit('increment')
+                //  console.log(this.$store.state.count)
+            },
+            setIndexProject(val) { this.IndexProject = parseInt(val) },
+            setIndexLang(val) { this.$store.commit('setILang', parseInt(val)) },
+            setUserAssign(val) {
+                this.UserAssign = val;
+                this.$store.commit('assignUser', val)
+            },
+            openForm(type) {
+                switch (type) {
+                    case 1:     // user
+                        const saveClose = (mItem) => {
+                            console.log('save close', mItem)
+                            this.UserInfo = mItem
+                        }
+                        const exitClose = (mItem) => {
+                            console.log('exit close', mItem)
+                        }
+                        this.$store.commit('setModal', [this.UserInfo, saveClose, exitClose])
+                        break;
+                }
             }
         },
+        // provide() {
+        //     return { }
+        // },
+        created() {
+            this.IndexProject = getRandomInt(0, this.$store.state.Projects.length)
+        },
+        beforeMount() {
+            // console.log('before mount', this)
+        },
+        mounted() { 
+            const pDom = document.querySelector(`.dnbimporthtml[dnbpath="${sPath}"]`)
+            if(pDom) pDom.remove()
+        },
+    })
+    app.use(dnbStore)
+    app.component('drop-select', DropSelect)
+    app.mount('#app')
 
-        // #region trace dev
-        message() { return this.$store.getters.message },
-        // #endregion
-    },
-    methods: {
-        selectPage(index) { this.IndexPage = index },
-        increment() {
-            this.$store.commit('increment')
-            //  console.log(this.$store.state.count)
-        },
-        setIndexProject(val) { this.IndexProject = parseInt(val) },
-        setIndexLang(val) { this.$store.commit('setILang', parseInt(val)) },
-        setUserAssign(val) {
-            this.UserAssign = val;
-            this.$store.commit('assignUser', val)
-        },
-        openForm(type) {
-            switch (type) {
-                case 1:     // user
-                    const saveClose = (mItem) => {
-                        console.log('save close', mItem)
-                        this.UserInfo = mItem
-                    }
-                    const exitClose = (mItem) => {
-                        console.log('exit close', mItem)
-                    }
-                    this.$store.commit('setModal', [this.UserInfo, saveClose, exitClose])
-                    break;
-            }
-        }
-    },
-    // provide() {
-    //     return { }
-    // },
-    created() {
-        this.IndexProject = getRandomInt(0, this.$store.state.Projects.length)
-    },
-    beforeMount() {
-        // console.log('before mount', this)
-    },
-    //mounted() { },
+    const appModal = createApp(AppModal)
+    appModal.use(dnbStore)
+    appModal.mount(`#app-modal`)
+
+}).catch(errStatus => {
+    console.log('Woop!', errStatus)
 })
-app.use(dnbStore)
-app.component('drop-select', DropSelect)
-app.mount('#app')
-
-const appModal = createApp(AppModal)
-appModal.use(dnbStore)
-appModal.mount(`#app-modal`)
