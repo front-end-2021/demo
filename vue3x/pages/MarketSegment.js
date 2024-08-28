4d26c97b8aac9a5e0756adc8aa423b56fece76dc
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2024-08-28 10:40:33 +0700

    code drag drop market vue3x

--- vue3x/pages/MarketSegment.js
+++ vue3x/pages/MarketSegment.js
@@ -83,40 +83,6 @@ const CellValuation = {
             if (item.Active) return `bi-check2-square`
             return `bi-square`
         },
-        clssCheckMark(mId, rId) {
-            const item = this.$store.getters.MarketRegionBy([mId, rId])
-            if (!item) return
-            if (item.Active) return `bi-check`
-        },
-        getBgColor(mId, rId) {
-            const item = this.$store.getters.MarketRegionBy([mId, rId])
-            if (!item) return
-            if (!item.Active) return { backgroundColor: `#eaeaea`, color: `#fff` }
-
-            const total = this.getValuation(mId, rId, item)
-            if (0 == total) return { backgroundColor: `#f0de4f`, color: `#fff` }
-
-            if (-11 < total && total < 0) return { backgroundColor: `#f9bb51`, color: `#fff` }
-            if (-21 < total && total < -10) return { backgroundColor: `#f2ad4d`, color: `#fff` }
-            if (-31 < total && total < -20) return { backgroundColor: `#e99c49`, color: `#fff` }
-            if (-41 < total && total < -30) return { backgroundColor: `#df8a44`, color: `#fff` }
-            if (-51 < total && total < -40) return { backgroundColor: `#d5773f`, color: `#fff` }
-            if (-61 < total && total < -50) return { backgroundColor: `#cb633a`, color: `#fff` }
-            if (-71 < total && total < -60) return { backgroundColor: `#e2a696`, color: `#000` }
-            if (-81 < total && total < -70) return { backgroundColor: `#e4a399`, color: `#000` }
-            if (-91 < total && total < -80) return { backgroundColor: `#e59d99`, color: `#000` }
-            if (-101 < total && total < -90) return { backgroundColor: `#e59494`, color: `#000` }
-            if (0 < total && total < 11) return { backgroundColor: `#c0d02e`, color: `#fff` }
-            if (10 < total && total < 21) return { backgroundColor: `#b8c92e`, color: `#fff` }
-            if (20 < total && total < 31) return { backgroundColor: `#adc02c`, color: `#fff` }
-            if (30 < total && total < 41) return { backgroundColor: `#a1b42b`, color: `#fff` }
-            if (40 < total && total < 51) return { backgroundColor: `#93a82a`, color: `#fff` }
-            if (50 < total && total < 61) return { backgroundColor: `#adc02c`, color: `#fff` }
-            if (60 < total && total < 71) return { backgroundColor: `#839b27`, color: `#fff` }
-            if (70 < total && total < 81) return { backgroundColor: `#668125`, color: `#fff` }
-            if (80 < total && total < 91) return { backgroundColor: `#597623`, color: `#fff` }
-            if (90 < total && total < 101) return { backgroundColor: `#4e6d22`, color: `#fff` }
-        },
     },
 }
 
@@ -138,7 +104,7 @@ export default {
         dragOptions() {
             return {
                 animation: 200,
-                group: "description",
+                group: "land",
                 disabled: false,
                 ghostClass: "ghost"
             }
@@ -288,17 +254,6 @@ export default {
             this.$root.openFormEditLand(land)
             setLastState(1, land.Id)
         },
-        getValuation(mId, rId, item) {
-            if (!item) item = this.$store.getters.MarketRegionBy([mId, rId])
-            if (!item) return;
-            return getTotal(item.Criterias)
-            function getTotal(items) {
-                if (!items.length) return
-                const lstCrt = items.map(x => x.Value * x.Weight)
-                const sum = lstCrt.reduce((a, b) => a + b, 0)
-                return Math.round(sum / lstCrt.length)
-            }
-        },
         onClickTab(e) {
             //console.log('on click tab', e.target, e)
             this.$root.clearPopupMenu(1)
@@ -443,10 +398,57 @@ export default {
             }
             this.$store.commit('setModal', [item, saveClose, xClose])
         },
+        getValuation(mId, rId, item) {
+            if (!item) item = this.$store.getters.MarketRegionBy([mId, rId])
+            if (!item) return;
+            return getTotal(item.Criterias)
+            function getTotal(items) {
+                if (!items.length) return
+                const lstCrt = items.map(x => x.Value * x.Weight)
+                const sum = lstCrt.reduce((a, b) => a + b, 0)
+                return Math.round(sum / lstCrt.length)
+            }
+        },
+        clssCheckMark(mId, rId) {
+            const item = this.$store.getters.MarketRegionBy([mId, rId])
+            if (!item) return
+            if (item.Active) return `bi-check`
+        },
+        getBgColor(mId, rId) {
+            const item = this.$store.getters.MarketRegionBy([mId, rId])
+            if (!item) return
+            if (!item.Active) return { backgroundColor: `#eaeaea`, color: `#fff` }
+
+            const total = this.getValuation(mId, rId, item)
+            if (0 == total) return { backgroundColor: `#f0de4f`, color: `#fff` }
+
+            if (-11 < total && total < 0) return { backgroundColor: `#f9bb51`, color: `#fff` }
+            if (-21 < total && total < -10) return { backgroundColor: `#f2ad4d`, color: `#fff` }
+            if (-31 < total && total < -20) return { backgroundColor: `#e99c49`, color: `#fff` }
+            if (-41 < total && total < -30) return { backgroundColor: `#df8a44`, color: `#fff` }
+            if (-51 < total && total < -40) return { backgroundColor: `#d5773f`, color: `#fff` }
+            if (-61 < total && total < -50) return { backgroundColor: `#cb633a`, color: `#fff` }
+            if (-71 < total && total < -60) return { backgroundColor: `#e2a696`, color: `#000` }
+            if (-81 < total && total < -70) return { backgroundColor: `#e4a399`, color: `#000` }
+            if (-91 < total && total < -80) return { backgroundColor: `#e59d99`, color: `#000` }
+            if (-101 < total && total < -90) return { backgroundColor: `#e59494`, color: `#000` }
+            if (0 < total && total < 11) return { backgroundColor: `#c0d02e`, color: `#fff` }
+            if (10 < total && total < 21) return { backgroundColor: `#b8c92e`, color: `#fff` }
+            if (20 < total && total < 31) return { backgroundColor: `#adc02c`, color: `#fff` }
+            if (30 < total && total < 41) return { backgroundColor: `#a1b42b`, color: `#fff` }
+            if (40 < total && total < 51) return { backgroundColor: `#93a82a`, color: `#fff` }
+            if (50 < total && total < 61) return { backgroundColor: `#adc02c`, color: `#fff` }
+            if (60 < total && total < 71) return { backgroundColor: `#839b27`, color: `#fff` }
+            if (70 < total && total < 81) return { backgroundColor: `#668125`, color: `#fff` }
+            if (80 < total && total < 91) return { backgroundColor: `#597623`, color: `#fff` }
+            if (90 < total && total < 101) return { backgroundColor: `#4e6d22`, color: `#fff` }
+        },
     },
     provide() {
         return {
-            getValuation: this.getValuation
+            getValuation: this.getValuation,
+            clssCheckMark: this.clssCheckMark,
+            getBgColor: this.getBgColor,
         }
     },
     //beforeCreate() { },
@@ -493,5 +495,17 @@ export default {
             this.setLandRegionMarket()
         })
     },
-    // mounted() {  },
+     mounted() {  
+        this.$root.DragDrop = {
+            direct: 'y',
+            options: {
+                animation: 200,
+                group: "market",
+                disabled: false,
+                ghostClass: "ghost"
+            },
+            type: 3     // market
+        }
+
+     },
 }
