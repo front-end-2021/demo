b85c5bc9e8447a455e480b2b9a109b622d4b1887
baodainguyen <baodaihd89@gmail.com>
2024-11-18 00:13:55 +0700

    sort goal, sub tab action plan in vue3x

--- vue3x/pages/ActionPlan.js
+++ vue3x/pages/ActionPlan.js
@@ -26,17 +26,17 @@ const ViewGoal = {
     },
     watch: {
         ListSub(subs, oSubs) {
-            console.group('watch list sub ', subs.map(x => {
-                return {
-                    Id: x.Id, ASort: x.ASort, GoalId: x.GoalId
-                }
-            }))
-            console.log('old subs ', oSubs.map(x => {
-                return {
-                    Id: x.Id, ASort: x.ASort, GoalId: x.GoalId
-                }
-            }))
-            console.groupEnd()
+            // console.group('watch list sub ', subs.map(x => {
+            //     return {
+            //         Id: x.Id, ASort: x.ASort, GoalId: x.GoalId
+            //     }
+            // }))
+            // console.log('old subs ', oSubs.map(x => {
+            //     return {
+            //         Id: x.Id, ASort: x.ASort, GoalId: x.GoalId
+            //     }
+            // }))
+            // console.groupEnd()
             if (subs.length == oSubs.length) {
                 if (subs.length < 2) return;
                 const nIds = subs.map(x => x.Id)
@@ -85,17 +85,86 @@ const ViewGoal = {
             }
         },
     },
-    methods: {
-        onEndDndSub(evt) {
-
+}
+const ViewProduct = {
+    template: `#tmp-comp-vw-prd`,
+    name: "ViewWrapPrd",
+    display: "ViewWrapPrd",
+    components: {
+        'comp-vw-goal': ViewGoal,
+        draggable: VueDraggableNext,
+    },
+    props: ['item', 'smpdid'],
+    data() {
+        return {
+            ListGoal: this.$store.getters.sortedItemsBy([9, [this.smpdid]])
+        }
+    },
+    computed: {
+        DndGoalOptions() {
+            return {
+                animation: 200,
+                disabled: false,
+                ghostClass: "ghost",
+                direction: 'vertical',
+                group: "goal",
+            }
+        },
+    },
+    watch: {
+        ListGoal(gols, oGols) {
+            if (gols.length == oGols.length) {
+                if (gols.length < 2) return;
+                const nIds = gols.map(x => x.Id)
+                const oIds = oGols.map(x => x.Id)
+                if (isDragDrop(oIds, nIds)) {
+                    const mSorts = oGols.map(x => x.ASort)
+                    for (let ii = 0; ii < gols.length; ii++) {
+                        gols[ii].ASort = mSorts[ii]
+                    }
+                }
+            } else if (oGols.length < gols.length) {
+                const smpId = this.smpdid
+                if (1 == gols.length && smpId != gols[0].SubmPrdId) {
+                    gols[0].SubmPrdId = smpId
+                    return
+                }
+                // 2 <= gols.length
+                const ii = getIiDnd(smpId)
+                if (-1 < ii) {
+                    const fulGols = this.$store.getters.sortedItemsBy([9, [this.smpdid]])
+                    const srcGol = gols[ii]
+                    srcGol.SubmPrdId = smpId
+                    if (0 == ii) {
+                        let ii1 = fulGols.findIndex(s => s.Id == gols[ii + 1].Id)
+                        fulGols.splice(ii1, 0, srcGol)       // insert
+                        let index = 1
+                        for (let ss = 0; ss < fulGols.length; ss++) {
+                            fulGols[ss].ASort = index++
+                        }
+                    } else {            // 1 <= ii
+                        let ii1 = fulGols.findIndex(s => s.Id == gols[ii - 1].Id)
+                        fulGols.splice(ii1 + 1, 0, srcGol)       // insert
+                        let index = 1
+                        for (let ss = 0; ss < fulGols.length; ss++) {
+                            fulGols[ss].ASort = index++
+                        }
+                    }
+                }
+                function getIiDnd(pId) {
+                    for (let ii = 0; ii < gols.length; ii++) {
+                        if (pId != gols[ii].SubmPrdId) return ii
+                    }
+                    return -1
+                }
+            }
         },
     },
 }
 export default {
     template: `#tmp-comp-action-plan`,
     components: {
-        'comp-vw-goal': ViewGoal,
-        draggable: VueDraggableNext,
+        'comp-vw-prd': ViewProduct,
     },
     data() {
         return {
@@ -202,12 +271,9 @@ export default {
                     subMk = lstSubMkXy[ss]
                     smPrId = `${subMk.Id}-${prId}`
                     if (groupedGoals.has(smPrId)) {
-                        const lstGoal = groupedGoals.get(smPrId)
-                        lstGoal.sort((a, b) => a.ASort - b.ASort)
                         const item = {
                             Entry: subMk,
                             SubmarketProductId: smPrId,
-                            Goals: lstGoal
                         }
                         lstSmk.push(item)
                     }
@@ -237,12 +303,9 @@ export default {
                     prd = products[pp]
                     smPrId = `${smId}-${prd.Id}`
                     if (groupedGoals.has(smPrId)) {
-                        const lstGoal = groupedGoals.get(smPrId)
-                        lstGoal.sort((a, b) => a.ASort - b.ASort)
                         const item = {
                             Entry: prd,
                             SubmarketProductId: smPrId,
-                            Goals: lstGoal
                         }
                         lst.push(item)
                     }
@@ -250,97 +313,10 @@ export default {
                 return lst;
             }
         },
-        onStartDndGoal(evt) {
 
-        },
-        onEndDndGoal(evt) {
-            const desGoalIds = []           // new ordered id
-            evt.to.querySelectorAll(`[goalid]`).forEach(gl => {
-                const gId = gl.getAttribute('goalid')
-                desGoalIds.push(parseInt(gId))
-            })
-            if (desGoalIds.length < 1) return;
-
-            const srcSmPrId = evt.from.getAttribute('smprid')
-            const desSmPrId = evt.to.getAttribute('smprid')
-            const srcGols = this.$store.getters.sortedItemsBy([9, [srcSmPrId]])
-            if (srcSmPrId == desSmPrId) {
-                // drag drop same sub-market product
-                let oldIds = srcGols.map(x => x.Id)
-                if (oldIds.join(',') != desGoalIds.join(',')) {
-                    this.$store.dispatch('updateAsort', [9, oldIds, desGoalIds]).then(items => {
-                        for (let ll = 0, itm, nItm; ll < srcGols.length; ll++) {
-                            itm = srcGols[ll]
-                            nItm = items.find(ld => ld.Id == itm.Id)
-                            itm.ASort = nItm.ASort
-                        }
-                    })
-                }
-            } else if (srcSmPrId != desSmPrId) {
-                // drag drop diff sub-market product 
-                let desGoals = this.$store.getters.sortedItemsBy([9, [desSmPrId]])
-                let srcGoal
-                // #region remove drag goal from src
-                for (let oo = srcGols.length - 1; -1 < oo; oo--) {
-                    srcGoal = srcGols[oo]
-                    if (desGoalIds.includes(srcGoal.Id)) {
-                        break;
-                    }
-                }
-                // #endregion
-                srcGoal.SubmPrdId = desSmPrId
-                if (1 == desGoalIds.length) {
-                    srcGoal.ASort = 1
-                } else {
-                    let iiDes = desGoals.findIndex(x => desGoalIds[0] === x.Id)
-                    if (iiDes < 0) {
-                        // src id = desGoalIds[0]
-                        let ii1 = desGoals.findIndex(x => desGoalIds[1] === x.Id)
-                        if (ii1 < 0) return;
-                        let index = desGoals[ii1].ASort     // m-index des
-                        srcGoal.ASort = index               // src = des
-                        desGoals.splice(ii1, 0, srcGoal)       // insert at
-                        for (let ii = ii1 + 1, desG; ii < desGoals.length; ii++) {
-                            desG = desGoals[ii]
-                            if (desG.ASort < ++index) {
-                                desG.ASort = index
-                            } else break;
-                        }
-                    } else {
-                        let ii1 = iiDes
-                        let ii = 1
-                        for (; ii < desGoalIds.length; ii++) {
-                            const jDes = desGoals.findIndex(x => desGoalIds[ii] === x.Id)
-                            if (jDes < 0) break
-                            ii1 = jDes
-                        }
-                        let index = desGoals[ii1].ASort     // m-index des
-                        srcGoal.ASort = ++index               // src = des + 1
-                        desGoals.splice(ii1 + 1, 0, srcGoal)       // insert at
-                        for (ii = ii1 + 2; ii < desGoals.length; ii++) {
-                            const desG = desGoals[ii]
-                            if (desG.ASort < ++index) {
-                                desG.ASort = index
-                            } else break;
-                        }
-                    }
-                }
-            }
-        },
     },
     created() {
         this.buildData([0], [0])
-    },
-    computed: {
-        DndOptions() {
-            return {
-                animation: 200,
-                disabled: false,
-                ghostClass: "ghost",
-                direction: 'vertical',
-                group: "goal",
-            }
-        },
     },
 
 }
