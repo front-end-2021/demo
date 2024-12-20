1fec5aa98891e6a00f5163f80ff074a6918c85b5
baodainguyen <baodaihd89@gmail.com>
2024-12-20 20:49:48 +0700

    code draw arrow extend in canvas project class-diagram

--- class-diagram/components/vw-diagram.js
+++ class-diagram/components/vw-diagram.js
@@ -1,10 +1,12 @@
+import { drawExtension } from "../mcanvas.js";
+
 export const ViewDiagram = {
     template: `#tmp-vw-diagram`,
     name: "View_Diagram",
     display: "View.Diagram",
     //inject: [''],
     //mixins: [],
-   // props: ['item'],
+    // props: ['item'],
     methods: {
         addTodo() {
             const lstT = this.item.Todos
@@ -12,9 +14,29 @@ export const ViewDiagram = {
         },
     },
     beforeUnmount() {
-        
+
     },
     mounted() {
+        let clsAccount = document.getElementById('cls-account')
+        let clsContact = document.getElementById('cls-contact')
+        const c = document.getElementById(`dnb-mcanvas`);
+        let offC = c.getBoundingClientRect()
+        const rootY = offC.top
+        const rootX = offC.left
+        let offAcc = clsAccount.getBoundingClientRect()
+        let offCon = clsContact.getBoundingClientRect()
+        let x0 = 60, y0 = 60;
+        let x1 = 200, y1 = 100;
+        x0 = offAcc.left + offAcc.width - rootX
+        y0 = offAcc.top + offAcc.height / 10 - rootX
         
+        x1 = offCon.left - rootY
+        y1 = offCon.top + offCon.height / 10 - rootY
+        var ctx = c.getContext("2d");
+        let p0 = [x0, y0];
+        let p1 = [x1, y1];
+
+        drawExtension.call(ctx, p0, p1, 8)
+      //  drawExtension.call(ctx, [80, 300], [120, 50], 8)
     },
 }
---
+++ class-diagram/mcanvas.js
@@ -0,0 +1,56 @@
+
+
+export function drawExtension(p0, p1, width) {
+    const ctx = this
+    let x1 = p1[0], y1 = p1[1];
+    let [a, b] = linearCoeffict(p0, p1);
+
+    let posE = directToD(p1, width * 3, a, b);
+    let xx0 = posE[0]
+    if (x1 < xx0) xx0 = posE[1]
+    let yy0 = a * xx0 + b;
+
+    ctx.beginPath();
+    ctx.arc(p0[0], p0[1], 1, 0, 2 * Math.PI);
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    ctx.moveTo(p0[0], p0[1]);
+    ctx.lineTo(xx0, yy0);
+
+    let [a1, b1] = perpendiLine([xx0, yy0], a, b);
+
+    let [xx1, xx2] = directToD([xx0, yy0], width, a1, b1);
+    let yy1 = a1 * xx1 + b1;
+    let yy2 = a1 * xx2 + b1;
+
+    ctx.moveTo(xx1, yy1);
+    ctx.lineTo(xx2, yy2);
+    ctx.moveTo(xx2, yy2);
+
+    ctx.lineTo(x1, y1);
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(xx1, yy1);
+    ctx.stroke();
+
+}
+function linearCoeffict(p0, p1) {
+    let x0 = p0[0], y0 = p0[1];
+    let x1 = p1[0], y1 = p1[1];
+    let a = (y1 - y0) / (x1 - x0);
+    return [a, y0 - a * x0]
+}
+function perpendiLine(p, a, b) {
+    let x = p[0], y = p[1];
+    let a1 = -1 / a;
+    return [a1, y - x * a1]
+}
+function directToD(p, d, a, b) {
+    let x = p[0], y = p[1];
+    let A = 1 + a * a;
+    let B = 2 * (a * b - x - a * y);
+    let C = x * x + y * y + b * b - d * d - 2 * y * b;
+    let x1 = (-B + Math.sqrt(B * B - 4 * A * C)) / (2 * A);
+    let x2 = (-B - Math.sqrt(B * B - 4 * A * C)) / (2 * A);
+    return [x1, x2]
+}
