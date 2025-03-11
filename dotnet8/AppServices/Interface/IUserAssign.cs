0d1b727a7f1425b1ef28ed559358884bf4404548
baodainguyen <baodaihd89@gmail.com>
2025-03-11 23:08:54 +0700

    remove unuse folder in demo

--- dotnet8/AppServices/Interface/IUserAssign.cs
+++
@@ -1,7 +0,0 @@
-using Web.Api.Client.Entries;
-
-namespace Web.Api.Services {
-    public interface IUserAssign {
-        Task<List<GoalInfo>> GetGoalsAssign();
-    }
-}
--- marketing-react/src/global/GlobalLog.js
+++
@@ -1,6 +0,0 @@
-export function logItem(item, title) {
-    if(title) {
-        return console.log(title, JSON.parse(JSON.stringify(item)))    
-    }
-    return console.log(JSON.parse(JSON.stringify(item)))
-}
